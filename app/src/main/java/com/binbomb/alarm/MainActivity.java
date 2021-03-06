726997c2f361b8294ca598e16662886f84d67c47
tonghoameo <tonghoameo@gmail.com>
2021-03-06 06:25:46 +0000

    first

---
+++ app/src/main/java/com/binbomb/alarm/MainActivity.java
@@ -0,0 +1,15 @@
+package com.binbomb.alarm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    //home/colab/AndroidStudioProjects/Alarm/app/src/main/res/raw/7722_15467-lq.ogg
+}
